ec7a71af6e50dfaf7c9503b26aae982fe4be12dd
Vicky BRS <viki2711@gmail.com>
2021-06-22 12:04:59 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,52 @@
+from turtle import Screen
+from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
+import time
+
+# Screen setup
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("My Snake Game")
+# Turn off the animation on the screen
+screen.tracer(0)
+
+# new snake object
+snake = Snake()
+food = Food()
+score = Scoreboard()
+
+# we want to know which keys are being pressed
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+# Move the snake forward
+# Update the animation at this point, after a move
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+
+    # Detect collision with food
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        score.add_score()
+
+    # Detect collision with wall
+    if snake.head.xcor() > 290 or snake.head.xcor() < -290 or snake.head.ycor() > 290 or snake.head.ycor() < -290:
+        game_is_on = False
+        score.game_over()
+
+    # Detect collision with tail
+    for seg in snake.turtles[1:]:
+        if snake.head.distance(seg) < 10:
+            game_is_on = False
+            score.game_over()
+
+screen.exitonclick()
---
+++ snake.py
@@ -0,0 +1,53 @@
+from turtle import Turtle
+
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DIST = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
+
+class Snake:
+    def __init__(self):
+        self.turtles = []
+        self.create_snake()
+        self.head = self.turtles[0]
+
+    # Turtle body
+    def create_snake(self):
+        for pos in STARTING_POSITIONS:
+            self.add_segment(pos)
+
+    def add_segment(self, position):
+        new_seg = Turtle(shape="square")
+        new_seg.color("white")
+        new_seg.penup()
+        new_seg.goto(position)
+        self.turtles.append(new_seg)
+
+    def extend(self):
+        self.add_segment(self.turtles[-1].position())
+
+    def move(self):
+        for i in range(len(self.turtles) - 1, 0, -1):
+            new_x = self.turtles[i - 1].xcor()
+            new_y = self.turtles[i - 1].ycor()
+            self.turtles[i].goto(new_x, new_y)
+        self.head.forward(MOVE_DIST)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
